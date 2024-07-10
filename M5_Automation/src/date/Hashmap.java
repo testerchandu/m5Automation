c2e34dd4e15b00ba90a9f0101976b3f2cf2b2909
Borra Naga Chandu <146701157+testerchandu@users.noreply.github.com>
2024-07-10 15:10:34 +0530

    Update Hashmap.java

--- M5_Automation/src/date/Hashmap.java
+++ M5_Automation/src/date/Hashmap.java
@@ -2,12 +2,25 @@ package date;
 
 import java.util.HashMap;
 
-public class Hashmap {
-	public static void main(String[] args) {
-		HashMap<String,Object>hmap=new HashMap();
-		hmap.put( "naga", "chandu");
-		hmap.put( "chandu","like");
-		System.out.println(hmap);
-		
+public class  juice_shop {
+	 public static void main(String[] args) throws AWTException, InterruptedException {
+    WebDriver driver=new ChromeDriver();
+    driver.get( "https://juice-shop.herokuapp.com/#/login");
+    driver.manage().window().maximize();
+    Robot r=new Robot();
+    r.keyPress( KeyEvent.VK_TAB);
+    r.keyRelease( KeyEvent.VK_TAB);
+    Thread.sleep(1000);
+    r.keyPress( KeyEvent.VK_TAB);
+    r.keyRelease( KeyEvent.VK_TAB);
+    Thread.sleep(1000);
+    r.keyPress( KeyEvent.VK_TAB);
+    r.keyRelease( KeyEvent.VK_TAB);
+    Thread.sleep(1000);
+    r.keyPress(KeyEvent.VK_ENTER);
+    r.keyRelease(KeyEvent.VK_ENTER);
+    driver.findElement(By.id( "email")).sendKeys( "nagachandu0649@gmail.com");
+    driver.findElement(By.id( "password")).sendKeys( "Chandu@123");
+    driver.findElement(By.xpath( "//button[@id='loginButton']")).click();
 	}
 }
